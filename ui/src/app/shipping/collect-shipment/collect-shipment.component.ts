64f6150a272d2e7b1011ee57b3cee47381ab91b4
naushada <naushad.dln@gmail.com>
2023-11-24 13:02:54 +0530

    Updated

--- ui/src/app/shipping/collect-shipment/collect-shipment.component.ts
+++ ui/src/app/shipping/collect-shipment/collect-shipment.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {formatDate} from '@angular/common';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Account, AppGlobals, AppGlobalsDefault } from 'src/common/app-globals';
+import { HttpsvcService } from 'src/common/httpsvc.service';
+import { PubsubsvcService } from 'src/common/pubsubsvc.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-collect-shipment',
   templateUrl: './collect-shipment.component.html',
   styleUrls: ['./collect-shipment.component.scss']
 })
-export class CollectShipmentComponent implements OnInit {
+export class CollectShipmentComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  collectShipmentForm: FormGroup;
+  
+  defValue?: AppGlobals;
+  isAutoGenerateState: boolean = true;
+  isAwbNoDisabled: boolean = true;
+
+  accountInfoList: Account[] = [];
+  loggedInUser?: Account;
+  subsink = new SubSink();
+
+  constructor(private fb: FormBuilder, private rt: Router, private http: HttpsvcService, private subject: PubsubsvcService) { 
+    this.defValue = {...AppGlobalsDefault};
+
+    this.subsink.sink = this.subject.onAccount.subscribe(rsp => {this.loggedInUser = rsp;},
+      error => {},
+      () => {});
+
+    this.subsink.sink = this.subject.onAccountList.subscribe(rsp => {
+      rsp?.forEach((elm: Account) => {this.accountInfoList.push(elm);});
+      },
+      error => {},
+      () => {});
+      
+      this.collectShipmentForm = this.fb.group({
+       from: this.fb.group({
+          accountNo:'',
+          when: formatDate(new Date(), 'dd/MM/yyyy', 'en-GB'),
+          country:this.defValue.CountryName?.at(1),
+          state:'',
+          city:'',
+          postalCode:'',
+          phoneNumber:'',
+          addreess: ''
+        })
+      })
+  }
 
   ngOnInit(): void {
+  }
+
+  onSchedulePickup() {
+
+  }
+
+  ngOnDestroy(): void {
+    this.subsink.unsubscribe();
   }
 
 }
